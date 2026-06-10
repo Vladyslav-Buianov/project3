de1a701d57b755c19623e5daf24bb0473c3a97a9
Polina-Mirgorodska <mirgorodskapolina@gmail.com>
2026-06-10 16:47:27 +0200

    final

--- src/js/age1.js
+++ src/js/age1.js
@@ -1,19 +1,3 @@
-// const form = document.querySelector(".age__form")
-// form.addEventListener("submit", (event)=>{
-//     event.preventDefault()
-//     const action = Number(event.currentTarget.elements[0].value)
-//     if(!Number.isNaN(action)){
-//         if (action % 4 === 0){
-//             console.log("Високосний");
-            
-//         }else{
-//             console.log("Не високосний");
-            
-//         }
-        
-//     }
-// })
-
 const textRef = document.querySelector('.age__text');
 const inputRef = document.querySelector('.age__input');
 const buttonRef = document.querySelector('.age__button');
@@ -23,9 +7,11 @@ function checkAge(age) {
 
   if ((age % 400 === 0) || (age % 4 === 0 && age % 100 !== 0)) {
     textRef.textContent = 'Ви народилися у високосний рік!';
+    textRef.style.color = '#039900'
     textRef.classList.add('agetrue');
   } else {
     textRef.textContent = 'Ви народилися не у високосний рік!';
+     textRef.style.color = '#900'
     textRef.classList.add('agefalse');
   }
 }
